acf67a18cc051ba5897b3e0988588a91d85d3e57
MrManu <superbrandon2018@gmail.com>
2021-11-27 17:41:39 -0600

    Seccion portafolio terminada

--- components/Works.js
+++ components/Works.js
@@ -6,48 +6,50 @@ import "swiper/css/navigation"
 import "swiper/css/pagination"
 
 import SwiperCore, {
-    Navigation,Pagination,Mousewheel,Keyboard
+    Navigation, Pagination, Mousewheel, Keyboard
 } from 'swiper';
-SwiperCore.use([Navigation,Pagination,Mousewheel,Keyboard]);
+SwiperCore.use([Navigation, Pagination, Mousewheel, Keyboard]);
 
 export default function Works() {
     return (
         <section className='portafolio'>
-            <Swiper cssMode={true} navigation={true} pagination={true} mousewheel={true} keyboard={true} className="portafolio__container mySwiper"> 
-                    <SwiperSlide className="portafolio__content">
-                        <img src="/img/portfolio1.jpg" alt=""/>
+            <Swiper cssMode={true} navigation={true} pagination={true} mousewheel={true} className="portafolio__container container swiper">
+                <div className="swiper-wrapper">
+                    <SwiperSlide className="portafolio__content grid">
+                        <img src="/img/portfolio1.jpg" className='portafolio__img' />
                         <div className="portafolio__data">
                             <h3 className='portafolio__title'>Google Clone</h3>
-                            <p className='portafolio__description'>Pagina Clone del buscador mas famoso del mundo, realizado con finalidades educativas</p>
-                            <a href="#" className='button portafolio__button'>
+                            <p className='portafolio__description'>El buscador de Google o buscador web de Google (en inglés Google Search) es un motor de búsqueda en la web propiedad y el principal producto de Google</p>
+                            <a href="https://google-clone-mhnecu5mh-mrmannye.vercel.app/" target='_blank' className='button portafolio__button'>
                                 Demo
                                 <i className='uil uil-arrow-right portafolio__icon'></i>
                             </a>
                         </div>
                     </SwiperSlide>
-                    <SwiperSlide className="portafolio__content">
-                        <img src="/img/portfolio2.jpg" alt=""/>
+                    <SwiperSlide className="portafolio__content grid">
+                        <img src="/img/portfolio2.jpg" className='portafolio__img' />
                         <div className="portafolio__data">
                             <h3 className='portafolio__title'>Netflix Clone</h3>
-                            <p className='portafolio__description'>Pagina Clone del buscador mas famoso del mundo, realizado con finalidades educativas</p>
-                            <a href="#" className='button portafolio__button'>
+                            <p className='portafolio__description'>Un servicio por suscripción estadounidense que opera a nivel mundial y cuyo servicio principal es la distribución de contenidos audiovisuales a través de una plataforma en línea o servicio de video bajo demanda por streaming</p>
+                            <a href="#" target='_blank' className='button portafolio__button'>
                                 Demo
                                 <i className='uil uil-arrow-right portafolio__icon'></i>
                             </a>
                         </div>
                     </SwiperSlide>
-                    <SwiperSlide className="portafolio__content">
-                        <img src="/img/portfolio3.jpg" alt=""/>
+                    <SwiperSlide className="portafolio__content grid">
+                        <img src="/img/portfolio3.jpg" className='portafolio__img' />
                         <div className="portafolio__data">
                             <h3 className='portafolio__title'>Hulu Clone</h3>
-                            <p className='portafolio__description'>Pagina Clone del buscador mas famoso del mundo, realizado con finalidades educativas</p>
-                            <a href="#" className='button portafolio__button'>
+                            <p className='portafolio__description'>Hulu latino, es un servicio de streaming similar a Netflix, donde el usuario paga una suscripción mensual de US $11,99 para poder acceder a todo el catálogo sin ningún tipo de anuncio publicitario.</p>
+                            <a href="https://hulu-clone-bempa1ga4-mrmannye.vercel.app/" target='_blank' className='button portafolio__button'>
                                 Demo
                                 <i className='uil uil-arrow-right portafolio__icon'></i>
                             </a>
                         </div>
                     </SwiperSlide>
-                </Swiper>
+                </div>
+            </Swiper>
         </section>
     )
 }
