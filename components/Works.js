7850667c86eda30173f33faa2258b6e862798f46
MrManu <superbrandon2018@gmail.com>
2021-11-27 01:05:19 -0600

    Carrusel de Portafolio

--- components/Works.js
+++ components/Works.js
@@ -1,45 +1,53 @@
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+import SwiperCore, {
+    Navigation,Pagination,Mousewheel,Keyboard
+} from 'swiper';
+SwiperCore.use([Navigation,Pagination,Mousewheel,Keyboard]);
 
 export default function Works() {
     return (
         <section className='portafolio'>
-            <div className="portafolio__container"> 
-                <div>
-                    <div className="portafolio__content">
+            <Swiper cssMode={true} navigation={true} pagination={true} mousewheel={true} keyboard={true} className="portafolio__container mySwiper"> 
+                    <SwiperSlide className="portafolio__content">
                         <img src="/img/portfolio1.jpg" alt=""/>
                         <div className="portafolio__data">
                             <h3 className='portafolio__title'>Google Clone</h3>
                             <p className='portafolio__description'>Pagina Clone del buscador mas famoso del mundo, realizado con finalidades educativas</p>
-                            <a href="#" className='button'>
+                            <a href="#" className='button portafolio__button'>
                                 Demo
-                                <i className='uil uil-arrow-right'></i>
+                                <i className='uil uil-arrow-right portafolio__icon'></i>
                             </a>
                         </div>
-                    </div>
-                    <div className="portafolio__content">
+                    </SwiperSlide>
+                    <SwiperSlide className="portafolio__content">
                         <img src="/img/portfolio2.jpg" alt=""/>
                         <div className="portafolio__data">
                             <h3 className='portafolio__title'>Netflix Clone</h3>
                             <p className='portafolio__description'>Pagina Clone del buscador mas famoso del mundo, realizado con finalidades educativas</p>
-                            <a href="#" className='button'>
+                            <a href="#" className='button portafolio__button'>
                                 Demo
-                                <i className='uil uil-arrow-right'></i>
+                                <i className='uil uil-arrow-right portafolio__icon'></i>
                             </a>
                         </div>
-                    </div>
-                    <div className="portafolio__content">
+                    </SwiperSlide>
+                    <SwiperSlide className="portafolio__content">
                         <img src="/img/portfolio3.jpg" alt=""/>
                         <div className="portafolio__data">
                             <h3 className='portafolio__title'>Hulu Clone</h3>
                             <p className='portafolio__description'>Pagina Clone del buscador mas famoso del mundo, realizado con finalidades educativas</p>
-                            <a href="#" className='button'>
+                            <a href="#" className='button portafolio__button'>
                                 Demo
-                                <i className='uil uil-arrow-right'></i>
+                                <i className='uil uil-arrow-right portafolio__icon'></i>
                             </a>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </SwiperSlide>
+                </Swiper>
         </section>
     )
 }
